35930365c9a46ac41fc0087fd406842e702d1621
tommyippoz <tommaso.zoppi@unifi.it>
2025-04-23 16:16:33 +0200

    pyod bench update

--- pyod-benchmark/benchmark.py
+++ pyod-benchmark/benchmark.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Benchmark of all implemented algorithms
 """
-# Author: Yue Zhao <zhaoy@cmu.edu>
+# Author: Yue Zhao  <zhaoy@cmu.edu>
 # License: BSD 2 clause
 
 from __future__ import division
